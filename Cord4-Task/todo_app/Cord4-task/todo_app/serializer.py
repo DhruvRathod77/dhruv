d1d11974768bcf4694bccf00415b38883abd9889
Dhruv Rathod <rathoddhruv77@gmail.com>
2022-09-23 11:51:38 +0530

    change

--- Cord4-Task/todo_app/Cord4-task/todo_app/serializer.py
+++
@@ -1,8 +0,0 @@
-from pyexpat import model
-from rest_framework import fields, serializers
-from .models import Todo
-
-class Todoserializer(serializers.ModelSerializer):
-    class Meta:
-        model = Todo
-        fields = '__all__'
